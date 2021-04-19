19d510a4de3824ebe28d97184e23cf9a957e8f38
D-Rayven <vincent.grenet@outlook.com>
2021-04-19 17:06:47 +0200

    fin chapitre 2

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,7 @@
 const API_TOKEN = "eb6fbf991e607cd4d48efc0ae3f24df9"
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_TOKEN + "&language=fr&query=" + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_TOKEN + "&language=fr&query=" + text + "&page=" + page
 
     return fetch(url)
         .then((response) => response.json())
--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, FlatList, Text } from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
@@ -10,15 +10,24 @@ class Search extends React.Component {
     constructor(props) {
         super(props)
         this.searchedText = ""
+        this.page = 0
+        this.total_pages = 0
         this.state = {
-            films: []
+            films: [],
+            isLoading: false,
         }
     }
 
     _loadFilms() {
         if (this.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-                this.setState({ films: data.results })
+            this.setState({ isLoading: true })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page + 1).then(data => {
+                this.page = data.page
+                this.total_pages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ],
+                    isLoading: false
+                })
             })
         }
     }
@@ -26,6 +35,26 @@ class Search extends React.Component {
     _searchedTextInputChanged(text) {
         this.searchedText = text
     }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={ styles.loading_container }>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+
+    _searchFilms() {
+        this.page = 0
+        this.total_pages = 0
+        this.setState({
+            films: []
+        })
+        this._loadFilms()
+    }
+
     render() {
         return (
             <View style={ styles.main_container }>
@@ -33,13 +62,20 @@ class Search extends React.Component {
                     style={ styles.textInput }
                     placeholder="Titre du film"
                     onChangeText={ (text) => this._searchedTextInputChanged(text) }
-                    onSubmitEditing={ () => this._loadFilms }/>
-                <Button title="Rechercher" onPress={() => this._loadFilms()}/>
+                    onSubmitEditing={ () => this._searchFilms }/>
+                <Button title="Rechercher" onPress={() => this._searchFilms()}/>
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
+                    onEndReachedThreshold={ 0.5 }
+                    onEndReached={ () => {
+                    if (this.page < this.total_pages) {
+                        this._loadFilms()
+                    }
+                }}
                 />
+                { this._displayLoading() }
             </View>
         )
     }
@@ -57,6 +93,15 @@ const styles = StyleSheet.create ({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
